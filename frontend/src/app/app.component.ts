dfaa6e21297702ed3b7779188de230221b0b146d
Jason Franklin <jjfrankl@ncsu.edu>
2024-11-23 16:38:43 -0500

    Adding frontend and backend setup

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { MapComponent } from './components/map/map.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'forage-for-cool-bees-frontend';
+  title = 'forage-for-cool-bees';
 }
